cef88e7d01b34f77d8bfcb84da1db7cf8c0475e9
Ivan Komarenko <ivankomarenko.work@gmail.com>
2024-03-21 23:43:05 +0200

    Added Unit Test project.

--- My4Notes.DatabaseAccess/ApplicationDbContext.cs
+++ My4Notes.DatabaseAccess/ApplicationDbContext.cs
@@ -5,14 +5,19 @@ namespace My4Notes.DatabaseAccess;
 
 public class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext()
+    private string _connectionString = "Host=localhost;Port=5433;Database=My4NotesDB;Username=postgres;Password=Str0ngP@ssw0rd";
+    
+    public ApplicationDbContext() {}
+        
+    public ApplicationDbContext(string connString)
     {
+        _connectionString = connString;
         Database.EnsureCreated();
     }
-    public DbSet<Note> Notes { get; set; }
+    public virtual DbSet<Note> Notes { get; set; }
 
     // TODO: expose secrets outside of the app as environmental variables
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-        optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=My4NotesDB;Username=postgres;Password=Str0ngP@ssw0rd");
+        optionsBuilder.UseNpgsql(_connectionString);
     }
 }
