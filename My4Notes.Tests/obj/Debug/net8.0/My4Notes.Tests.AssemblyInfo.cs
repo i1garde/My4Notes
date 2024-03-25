8776c94efaaec43269f1a25281bbea285a4f2b65
Ivan Komarenko <ivankomarenko.work@gmail.com>
2024-03-25 17:38:24 +0200

    Deleted duplicated init migration from Migrations folder

--- My4Notes.Tests/obj/Debug/net8.0/My4Notes.Tests.AssemblyInfo.cs
+++ My4Notes.Tests/obj/Debug/net8.0/My4Notes.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My4Notes.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08fcf6fb1301671d79f8610dce877609bdcb67c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90003e7301380b74654e45ee236cfdab51537a9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("My4Notes.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My4Notes.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
